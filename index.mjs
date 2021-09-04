dbb038d76f0b3fe9b3957c8f8e142a4e4b7b19eb
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-04 15:22:16 -0700

    modules still not working

--- index.mjs
+++ index.mjs
@@ -1,10 +1,24 @@
 import { default as romNumConv } from './Roman-Numeral-module.mjs';
 
-console.log(romNumConv(27));
-/*
+console.log(romNumConv(989786));
+
 const output = document.getElementById("roman-out");
 const inputBox = document.getElementById("num-in");
 
+/*
+const span = document.getElementById("test-span");
+const textTest = document.createTextNode("testing 1,2,3");
+span.appendChild(textTest);
+*/
+function changeText(userInput){
+    output.textContent = userInput;
+    output.innnerText = userInput;
+}
+
+inputBox.addEventListener('change', changeText);
+
+
+/*
 function updateRoman(){
     console.log("inside event listener");
     console.log(inputBox.value);
@@ -12,3 +26,4 @@ function updateRoman(){
     console.log("STILL inside event listener");
 }
 inputBox.addEventListener('change', updateRoman);*/
+
