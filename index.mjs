d9bb6fee02f7db047c62073a7105b8da1284b732
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-04 13:23:49 -0700

    launch configured, and modules set

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import { default as romNumConv } from './Roman-Numeral-module.mjs';
+
+console.log(romNumConv(27));
+/*
+const output = document.getElementById("roman-out");
+const inputBox = document.getElementById("num-in");
+
+function updateRoman(){
+    console.log("inside event listener");
+    console.log(inputBox.value);
+    output.textContent = romNumConv(inputBox.value);
+    console.log("STILL inside event listener");
+}
+inputBox.addEventListener('change', updateRoman);*/
