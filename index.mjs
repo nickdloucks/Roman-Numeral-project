6c68cee09e4477aa29aa7650a4f533c00cda2007
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-06 14:52:05 -0700

    data passes thru event listener and modules

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import { romNumConv } from './Roman-Numeral-module.mjs';
-//document.addEventListener("DOMContentLoaded", function() {
 
 
 console.log(romNumConv(46) + " in index.mjs");
@@ -8,32 +7,7 @@ console.log(romNumConv(46) + " in index.mjs");
 const inputBox = document.getElementById("num-in");
 const output = document.getElementById("roman-out");
 
-
-/*
-const span = document.getElementById("test-span");
-const textTest = document.createTextNode("testing 1,2,3");
-span.appendChild(textTest);
-*/
-function changeText(userInput){
-    //const textTest = document.createTextNode(userInput);
-    //output.appendChild(textTest);
-    //Event.preventDefault()
+window.changeText = function changeText(userInput){
     output.textContent = userInput;
     console.log("text has been changed by the changeText event Listener");
 }
-
-inputBox.addEventListener('input', changeText(inputBox.value));
-
-
-/*
-function updateRoman(){
-    console.log("inside event listener");
-    console.log(inputBox.value);
-    output.textContent = romNumConv(inputBox.value);
-    console.log("STILL inside event listener");
-}
-inputBox.addEventListener('change', updateRoman);*/
-
-//"testing 1,2,3"
-
-//});
