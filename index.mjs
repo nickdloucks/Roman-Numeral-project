25c42e4f240e3b5bffa1463578b8e01f988879b4
Nick Loucks <nick.d.loucks@gmail.com>
2021-09-06 13:14:13 -0700

    tinkering with modules

--- index.mjs
+++ index.mjs
@@ -2,7 +2,8 @@ import { romNumConv } from './Roman-Numeral-module.mjs';
 //document.addEventListener("DOMContentLoaded", function() {
 
 
-console.log(romNumConv(95));
+console.log(romNumConv(46) + " in index.mjs");
+
 
 const inputBox = document.getElementById("num-in");
 const output = document.getElementById("roman-out");
@@ -14,12 +15,14 @@ const textTest = document.createTextNode("testing 1,2,3");
 span.appendChild(textTest);
 */
 function changeText(userInput){
-    const textTest = document.createTextNode(userInput);
-    output.appendChild(textTest);
-    //output.textContent = userInput;
+    //const textTest = document.createTextNode(userInput);
+    //output.appendChild(textTest);
+    //Event.preventDefault()
+    output.textContent = userInput;
+    console.log("text has been changed by the changeText event Listener");
 }
 
-inputBox.addEventListener('change', changeText("hi"));
+inputBox.addEventListener('input', changeText(inputBox.value));
 
 
 /*
